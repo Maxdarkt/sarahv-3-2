b933d13b3dd20547751284b69972c4d660f32925
Maxence Tourneux <tourneuxmaxence@gmail.com>
2023-05-27 19:47:05 +0200

    modify slide 1 and add a slide 2 to modify text as pdf file

--- js/miniSlides.js
+++ js/miniSlides.js
@@ -1,61 +1,3 @@
-var dev = 1
-//----------Arret du slide en mode DEV------------------//
-//Partie du code à commenter en mode Production ->
-// $(".mini-slide").on("click", () => {
-//     if (dev == 0) {
-//         $(".mini-slide_1").show()
-//         $(".mini-slide_2").hide()
-//         $(".mini-slide_3").hide()
-//         $(".mini-slide_4").hide()
-//         $(".mini-slide_5").hide()
-//         $(".mini-slide_6").hide()
-//         dev++
-//     } else if (dev == 1) {
-//         $(".mini-slide_1").hide()
-//         $(".mini-slide_2").show()
-//         $(".mini-slide_3").hide()
-//         $(".mini-slide_4").hide()
-//         $(".mini-slide_5").hide()
-//         $(".mini-slide_6").hide()
-//         dev++
-//     } else if (dev == 2) {
-//         $(".mini-slide_1").hide()
-//         $(".mini-slide_2").hide()
-//         $(".mini-slide_3").show()
-//         $(".mini-slide_4").hide()
-//         $(".mini-slide_5").hide()
-//         $(".mini-slide_6").hide()
-//         dev++
-//     } else if (dev == 3) {
-//         $(".mini-slide_1").hide()
-//         $(".mini-slide_2").hide()
-//         $(".mini-slide_3").hide()
-//         $(".mini-slide_4").show()
-//         $(".mini-slide_5").hide()
-//         $(".mini-slide_6").hide()
-//         dev++
-//     } else if (dev == 4) {
-//         $(".mini-slide_1").hide()
-//         $(".mini-slide_2").hide()
-//         $(".mini-slide_3").hide()
-//         $(".mini-slide_4").hide()
-//         $(".mini-slide_5").show()
-//         $(".mini-slide_6").hide()
-//         dev++
-//     } else if (dev == 5) {
-//         $(".mini-slide_1").hide()
-//         $(".mini-slide_2").hide()
-//         $(".mini-slide_3").hide()
-//         $(".mini-slide_4").hide()
-//         $(".mini-slide_5").hide()
-//         $(".mini-slide_6").show()
-//         dev = 0
-//     }
-// })//<-- A commenter en mode Production
-//----------FIN Arret du slide en mode DEV------------------//
-
-//---------- Programme des mini-slide : Production --------//
-
 // Fonction génrale - Interval + pause
 $(document).ready(function () {
     //On cache toutes les autres div
@@ -68,6 +10,8 @@ $(document).ready(function () {
       $("#dots-3 > span").removeClass("dots-active")
       $(".mini-slide_4").hide()
       $("#dots-4 > span").removeClass("dots-active")
+      $(".mini-slide_5").hide()
+      $("#dots-5 > span").removeClass("dots-active")
       $(".mini-slide_" + slide).show()
       $("#dots-" + slide + " > span").addClass("dots-active")
     }
@@ -94,36 +38,6 @@ $(document).ready(function () {
         }
       })
     }
-    // timer pure des miniSlide
-    // const timer = (slide = 1) => {
-    //   if (!isPaused) {
-    //     if (slide == 1) {
-    //         $(".mini-slide_1").fadeOut(0)
-    //         $("#dots-1 > span").removeClass("dots-active")
-    //         $(".mini-slide_2").fadeIn(500)
-    //         $("#dots-2 > span").addClass("dots-active")
-    //         slide++
-    //     } else if (slide == 2) {
-    //         $(".mini-slide_2").fadeOut(0)
-    //         $("#dots-2 > span").removeClass("dots-active")
-    //         $(".mini-slide_3").fadeIn(500)
-    //         $("#dots-3 > span").addClass("dots-active")
-    //         slide++
-    //     } else if (slide == 3) {
-    //         $(".mini-slide_3").fadeOut(0)
-    //         $("#dots-3 > span").removeClass("dots-active")
-    //         $(".mini-slide_4").fadeIn(500)
-    //         $("#dots-4 > span").addClass("dots-active")
-    //         slide++
-    //     } else if (slide == 4) {
-    //         $(".mini-slide_4").fadeOut(0)
-    //         $("#dots-4 > span").removeClass("dots-active")
-    //         $(".mini-slide_1").fadeIn(500)
-    //         $("#dots-1 > span").addClass("dots-active")
-    //         slide = 1
-    //     }
-    //   }
-    // }
 
     // we init slide to 1
     var slide = 1
@@ -151,10 +65,16 @@ $(document).ready(function () {
         } else if (slide == 4) {
             $(".mini-slide_4").fadeOut(0)
             $("#dots-4 > span").removeClass("dots-active")
-            $(".mini-slide_1").fadeIn(500)
-            $("#dots-1 > span").addClass("dots-active")
-            slide = 1
-        }
+            $(".mini-slide_5").fadeIn(500)
+            $("#dots-5 > span").addClass("dots-active")
+            slide++
+        } else if (slide == 5) {
+          $(".mini-slide_5").fadeOut(0)
+          $("#dots-5 > span").removeClass("dots-active")
+          $(".mini-slide_1").fadeIn(500)
+          $("#dots-1 > span").addClass("dots-active")
+          slide = 1
+      }
       }
     }
     // on stocke la fonction setInterval
@@ -170,6 +90,7 @@ $(document).ready(function () {
     
     // Algorithme pour délcencher le timer, gérer les actions (prev, net, goTo)
     const miniSlide = (goTo, action) => {
+      isPaused = false
       if(goTo) {
         // on stoppe l'interval de temps
         launchTimer(false)
@@ -198,6 +119,7 @@ $(document).ready(function () {
         // on relance le timer
         launchTimer()
       } else {
+        initSlide()
         launchTimer()
       }
     }
@@ -205,15 +127,14 @@ $(document).ready(function () {
     //Déclenchement du timer
     var removeScrollBis = true
     window.onscroll = function () {
-        if (document.documentElement.scrollTop > 130 && removeScrollBis) {
-          isPaused = false
-          initSlide()
-          miniSlide()
-          removeScrollBis = false
-        }
+      if (document.documentElement.scrollTop > 130 && removeScrollBis) {
+        isPaused = false
+        miniSlide()
+        removeScrollBis = false
+      }
     }
 
-    // ACTION 
+    // EVENT click on button 
     $(".slide-prev").on("click", function() {
       miniSlide(null, -1)
     })
@@ -232,4 +153,20 @@ $(document).ready(function () {
     $("#dots-4").on("click", function() {
       miniSlide(4)
     })
+    $("#dots-5").on("click", function() {
+      miniSlide(5)
+    })
+//------------------------------------------------------//
+//----------Arret du slide en mode DEV------------------//
+//------------------------------------------------------//
+// Partie du code à commenter en mode Production ->
+  // var dev = 1
+  // initSlide()
+  // $(".mini-slide").on("click", () => {
+  //   dev = dev++ !== 5 ? dev++ : 1
+  //   initSlide(dev)
+  //   console.log(dev)
+  // })
+// Partie du code à commenter en mode Production
+//----------FIN Arret du slide en mode DEV------------------//
 })
